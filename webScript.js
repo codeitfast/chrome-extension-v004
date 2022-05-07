dd1d285271a28a09e0cb9a5de726847076ad02e0
codeitfast <74981041+codeitfast@users.noreply.github.com>
2022-05-07 00:52:54 +0000

    Initial commit

---
+++ webScript.js
@@ -0,0 +1,22 @@
+/*function documentEvents(){
+  var r = document.querySelector(':root');
+  
+  function reload(){
+    var color = document.getElementById('background').value
+    
+    r.style.setProperty('--bg', color);
+
+
+    
+    document.body.style.backgroundImage = "linear-gradient(to right, #2E3192,  " + color + ")"
+
+    //console.log(getComputedStyle(r).getPropertyValue('--bg'))
+    
+    if(Math.random(0,1) < .1){
+      console.log(color)
+    }
+    window.requestAnimationFrame(reload)
+  }
+  
+  window.requestAnimationFrame(reload)
+}*/
